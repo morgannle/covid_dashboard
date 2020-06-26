1de1508e28f21763834ff901792c67889803bf12
howilearnstatistics <nghiale5991@gmail.com>
2020-06-25 21:37:44 -0500

    Update prototype.R

--- prototype.R
+++ prototype.R
@@ -23,32 +23,36 @@ states_new_cases_death
 #this object contains visualization of each states
 states_cases_death_visualization = list()
 
+#this function takes data as input and plot an output
+draw = function(x){
+  temp_state = as.data.frame(x)
+  temp_state$date = as.Date(temp_state$date)
+  temp_fig = plot_ly(
+    x = temp_state$date,
+    y = temp_state$cases,
+    name = "Infected",
+    type = 'scatter',
+    mode = 'lines',
+    hoverinfo = 'text',
+    text = ~paste('</br> Day: ', temp_state$date,
+                  '</br> Total number of covid-19 Positive: ', temp_state$cases))
+  temp_fig = temp_fig %>% add_trace(
+    x = temp_state$date,
+    y = temp_state$death,
+    name = "Fatality",
+    type = 'scatter',
+    mode = 'lines',
+    text = ~paste('Total number of fatality: ', temp_state$death))
+  temp_fig = temp_fig %>% layout(hovermode = 'x')
+  return(plotly_build(temp_fig)) 
+}
+
 for (i in 1:state_count){
   states_cases_death[[i]] = us_cases_death[us_cases_death$state == state_name[i], ]
-  
-  temp_state = as.data.frame(states_cases_death[[i]])
-  temp_state$date = as.Date(temp_state$date)
-  
-  temp_fig = plot_ly(
-                     x = temp_state$date,
-                     y = temp_state$cases,
-                     name = "Infected",
-                     type = 'scatter',
-                     mode = 'lines',
-                     hoverinfo = 'text',
-                     text = ~paste('</br> Day: ', temp_state$date,
-                                   '</br> Total number of covid-19 Positive: ', temp_state$cases))
-  temp_fig = temp_fig %>% add_trace(
-                                    x = temp_state$date,
-                                    y = temp_state$death,
-                                    name = "Fatality",
-                                    type = 'scatter',
-                                    mode = 'lines',
-                                    text = ~paste('Total number of fatality: ', temp_state$death))
-  temp_fig = temp_fig %>% layout(hovermode = 'x')
-  
-  states_cases_death_visualization[[i]] = plotly_build(temp_fig)
-  }
+}
+
+states_cases_death_visualization = lapply(states_cases_death, draw)
+
 
 
 
