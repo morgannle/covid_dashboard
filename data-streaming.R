09fdf938b1ca95215df6e4b6df9a075b4c51d303
howilearnstatistics <nghiale5991@gmail.com>
2020-08-10 16:48:43 -0500

    8/10/2020
    Fix national heatmap, it now shows average daily cases per 100k.

--- data-streaming.R
+++ data-streaming.R
@@ -6,6 +6,7 @@ county_file_url = "https://raw.githubusercontent.com/nytimes/covid-19-data/maste
 state_file_url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
 us_file_url     = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
 fatality_by_gender_url = "http://data.cdc.gov/resource/9bhg-hcku.csv?$limit=10000&$$app_token=Y21ef2T1w3Ub7VVJAF8l3sGGd"
+population_by_county_url = "https://raw.githubusercontent.com/nytimes/covid-19-data/63d02b9d1073eff62827daf155a4fe1ef4ab7188/pop_est_2019.csv"
 
 covid19_county_data = read_csv(
   url(county_file_url)
@@ -15,9 +16,12 @@ covid19_nation_data = read_csv(
   )
 covid19_state_data = read_csv(
   url(state_file_url)
-)
+  )
 fatality_by_gender = read_csv(
   url(fatality_by_gender_url)
+  )
+population_by_county = read_csv(
+  url(population_by_county_url)
   )
 
 covid19_county_data$date = as.Date(covid19_county_data$date)
@@ -25,31 +29,36 @@ covid19_nation_data$date = as.Date(covid19_nation_data$date)
 covid19_county_data$fips = as.character(covid19_county_data$fips)
 
 temp_data = covid19_county_data
+
 temp_data[ ,c("X", "deaths")] = NULL
 temp_data = temp_data[complete.cases(temp_data), ]
 temp_data = temp_data %>% pivot_wider(names_from = date, values_from = cases)
 index = ncol(temp_data)
-temp_growth_percent = list()
+temp_new_cases = list()
 for(i in (index - 7):index){
-  temp_growth_percent[[i]] = (temp_data[,i] - temp_data[,i-1])*100/temp_data[,i-1]
-  
-}
-growth_percent = data.frame(temp_growth_percent[[index-6]],
-                            temp_growth_percent[[index-5]],
-                            temp_growth_percent[[index-4]],
-                            temp_growth_percent[[index-3]],
-                            temp_growth_percent[[index-2]],
-                            temp_growth_percent[[index-1]],
-                            temp_growth_percent[[index]])
+  temp_new_cases[[i]] = temp_data[,i] - temp_data[,i-1]
+  }
+new_cases = data.frame(temp_new_cases[[index-6]],
+                            temp_new_cases[[index-5]],
+                            temp_new_cases[[index-4]],
+                            temp_new_cases[[index-3]],
+                            temp_new_cases[[index-2]],
+                            temp_new_cases[[index-1]],
+                            temp_new_cases[[index]])
+total_cases = temp_data[ ,ncol(temp_data)]
+new_cases_7days_average = rowMeans(new_cases)
 
-growth_percent_7days_average = rowMeans(growth_percent)
-growth_percent_by_county = data.frame(temp_data$state,
-                                      temp_data$county,
-                                      temp_data$fips,
-                                      growth_percent_7days_average,
-                                      row.names = NULL)
-colnames(growth_percent_by_county) = c("State", "County", "fips", "7_day_average_case_growth")
-  
+new_cases_by_county = data.frame(temp_data$state,
+                                 temp_data$county,
+                                 temp_data$fips,
+                                 new_cases_7days_average,
+                                 total_cases,
+                                 row.names = NULL)
+colnames(new_cases_by_county) = c("State", "County", "fips", "new_cases_7day_average", "total_cases")
+new_cases_per_100k = merge(new_cases_by_county, population_by_county, by = "fips")
+new_cases_per_100k$new_cases_7day_average[new_cases_per_100k$new_cases_7day_average < 0] = 0
+new_cases_per_100k = new_cases_per_100k %>% mutate(new_cases_per_100k = new_cases_7day_average*100000/population,
+                                                   total_cases_per_100k = total_cases*100000/population)
 write.csv(covid19_nation_data,
           file = "C:/Users/nghia/OneDrive/Documents/GitHub/covid_dashboard/covid19_nation_data.csv")
 write.csv(covid19_state_data,
@@ -58,5 +67,5 @@ write.csv(covid19_county_data,
           file = "C:/Users/nghia/OneDrive/Documents/GitHub/covid_dashboard/covid19_county_data.csv")
 write.csv(fatality_by_gender,
           file = "C:/Users/nghia/OneDrive/Documents/GitHub/covid_dashboard/fatality_by_gender.csv")
-write.csv(growth_percent_by_county,
-          file = "C:/Users/nghia/OneDrive/Documents/GitHub/covid_dashboard/growth_percent_by_county.csv")
+write.csv(new_cases_per_100k,
+          file = "C:/Users/nghia/OneDrive/Documents/GitHub/covid_dashboard/new_cases_by_county.csv")
